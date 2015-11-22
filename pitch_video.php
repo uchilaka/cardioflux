dd9701bf7da53e586dc147175b83865089806c7b
Uchenna Chilaka <ndokoba@gmail.com>
2015-11-22 15:16:57 -0500

    updates

---
+++ pitch_video.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CardioFlux : Pitch Video</title>
+        <link rel="stylesheet" href="libs/bootstrap/dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="libs/bootstrap/dist/css/bootstrap-theme.min.css" />
+    </head>
+    <body>
+        <div class="jumbotron" style="text-align: center;">
+            <h3>Heart Health Matters</h3>
+            <iframe style="display: block; margin: 0 auto;" width="640" height="480" src="https://www.youtube.com/embed/4VP1kKHLRhA" frameborder="0" allowfullscreen></iframe>        
+        </div>
+    </body>
+</html>
