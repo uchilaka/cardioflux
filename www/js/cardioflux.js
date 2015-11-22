5ab455c9b1603f7c37f550a221dcd0958bdc2924
Uchenna Chilaka <ndokoba@gmail.com>
2015-11-21 21:23:20 -0500

    Nothing to see here. Just committing progress...

--- www/js/cardioflux.js
+++ www/js/cardioflux.js
@@ -1,6 +1,7 @@
 var CardioFlux = angular.module('CardioFlux', [
     'ngRoute',
     'ngSanitize',
+    'svc.context',
 ], function ($httpProvider) {
     // Use x-www-form-urlencoded Content-Type
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
